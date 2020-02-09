ddceb617f46cb42c17300dfe7d9b40c723b6cb82
PabloSSena <58041720+PabloSSena@users.noreply.github.com>
2020-02-09 12:07:27 -0300

    Add files via upload

--- src/Change.js
+++ src/Change.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import './globalCad.css';
 import './global.css';
+import FormRegister from './FormRegister'
+
 
 function Change(){
     return (
-      <body>
-        
-        <div className = "header">
-          <div className="inner_header"></div>
-        </div>
-           
-        <div className="barra1">
-          <h1>.</h1>
-        </div>
+      <body> 
+      <div className = "header">
+        <div className="inner_header"></div>
+      </div>
+         
+      <div className="barra1">
+        <h1>.</h1>
+      </div>
 
-        <div className="barra3">
-          <h1>.</h1>
+      <div className="barra3">
+        <h1>.</h1>
+      </div>
+      
+      <div className="quadro">
+       <div className = "header">
+        <div className="inner_header">
+          <strong>Alterar</strong>
         </div>
-
-        <div className="barra2">
-          <h1>.</h1>
-         </div> 
-
-      </body>
+       </div>
+      
+       <FormRegister/> 
+      
+      </div>
+      <div className="barra2">
+        <h1>.</h1>
+       </div> 
+       
+    </body>
       
         )
     }
--- src/Excluir.js
+++ src/Excluir.js
@@ -2,29 +2,51 @@ import React from 'react';
 import './globalCad.css';
 
 function Excluir(){
-    return (
-      <body>
+    return(
+    <body>
         
-        <div className = "header">
-          <div className="inner_header"></div>
-        </div>
-           
-        <div className="barra1">
-          <h1>.</h1>
-        </div>
+    <div className = "header">
+      <div className="inner_header"></div>
+    </div>
+       
+    <div className="barra1">
+      <h1>.</h1>
+    </div>
 
-        <div className="barra3">
-          <h1>.</h1>
-        </div>
+    <div className="barra3">
+      <h1>.</h1>
+    </div>
 
-        <div className="barra2">
-          <h1>.</h1>
-         </div> 
+    <div className="quadro">
+    <div className = "header">
+      <div className="inner_header">
+        <strong>DIGITE O ID DO USUARIO</strong>
+      </div>
+    </div>
 
-      </body>
-      
-        )
-    }
+    <form>
+      <div className="inputs">
+        <label htmlFor="idPesquisar">ID do usuario</label>
+        <input name="idPesquisar" id="idPesquisar" required/>
+      </div>
+
+      <div className="inputs"> 
+          <button onClick>Excluir</button>
+      </div>       
+
+    </form>     
+  
+
+    </div>
+
+    <div className="barra2">
+      <h1>.</h1>
+     </div> 
+
+  </body>
+  
+    )
+}
   
     
     export default Excluir;
